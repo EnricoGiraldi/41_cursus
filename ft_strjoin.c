0a40d53281c68663d39b83d4e39d5f81217809ee
Enrico Giraldi <97803162+EnricoGiraldi@users.noreply.github.com>
2023-01-28 16:23:02 +0100

    Add files via upload

---
+++ ft_strjoin.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: engirald <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/17 18:20:53 by engirald          #+#    #+#             */
+/*   Updated: 2023/01/17 19:20:02 by engirald         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len1;
+	size_t	len2;
+	char	*result;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	result = malloc(len1 + len2 + 1);
+	ft_memcpy(result, s1, len1);
+	ft_memcpy(result + len1, s2, len2 + 1);
+	return (result);
+}
